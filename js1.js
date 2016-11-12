9830b89e90646fe4434999a474265eb8734e654f
MinasAbrahamyan <minas.a@gmail.com>
2016-11-12 20:11:57 +0400

    simple js things

---
+++ js1.js
@@ -0,0 +1,20 @@
+/*if (true) {
+	let a= 123;
+	console.log("console-log1="+a);
+}
+//console.log("console-log2="+a);
+//a*/
+
+/*
+for(var i=0; i<3; i++) {
+	console.log(i);
+}
+console.log("end_i="+i); 
+100
+* /
+const a= 123;
+//a= "Hello";
+b= 'abcde';
+*/
+const let a= 123;
+
